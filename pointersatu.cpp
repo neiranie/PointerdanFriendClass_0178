7fd993691b44b28f54b42f2f46ac77edf0e70f93
neiranie <nurkhairanianneira@gmail.com>
2025-05-05 16:23:38 +0700

    coding fix

--- pointersatu.cpp
+++ pointersatu.cpp
@@ -5,16 +5,22 @@ class mahasiswa{
     public:
     int nim;
     void showNim(){
-    cout<<"No Induk = "<<nim<<endl;
+        cout<<"No Induk = "<<nim<<endl;
     }
 };
 
-int main()
+int main(){
 
-mahasiswa mhs; //Object mhs
-mhs.nim = 5;
-mhs.showNim(); //Member Access Operator
+    mahasiswa mhs; //Object mhs
+    mhs.nim = 5;
+    mhs.showNim(); //Member Access Operator
 
-mahasiswa &refMhs = mhs; //Pointer Reference refMhs
-mhs.nim = 2; //Member Access Operator
+    mahasiswa &refMhs = mhs; //Pointer Reference refMhs
+    mhs.nim = 2; //Member Access Operator
+    mhs.showNim();
 
+    mahasiswa *pMhs = &mhs; //pointer dereferences pMhs
+    pMhs->nim = 3; //Arrow Operator
+    pMhs->showNim();
+    return 0;
+}
